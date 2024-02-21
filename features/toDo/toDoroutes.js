65c3cbf855dbb57073a8a9fef38fe98710e1dde9
ES-3508 <erandasanjaya@gmail.com>
2024-02-21 05:48:37 +0530

    Feat: Dummy auth middleware

--- features/toDo/toDoroutes.js
+++ features/toDo/toDoroutes.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const todoController = require('./toDoController');
 const dummyAuthMiddleware = require('../../middleware/dummyMiddleware');
 
+// protected route with dummy middleware
 router.get('/',dummyAuthMiddleware, todoController.getAllTodos);
+
+// public routes
 router.get('/:id', todoController.getTodoById);
 router.post('/', todoController.createTodo);
 router.put('/:id', todoController.updateTodo);
